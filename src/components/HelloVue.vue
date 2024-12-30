de936c766ed107cf6da580ea9d61ac455ea085e5
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-30 21:15:44 +0700

    Directive Binding

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1 :id="id" v-bind:class="classHello">{{ hello }}</h1>
-  <div :class="classHello" v-html="hello"></div>
+  <h1 v-bind="data">{{ hello }}</h1>
+  <div :class="data.class" v-html="hello"></div>
   <button :disabled="buttonDisabled">This is button</button>
 </template>
 
 <script setup>
 console.log("Hello Vue");
 const hello = "<b>Hello Vue</b>";
-const classHello = "hello";
-const id = "hello";
+const data = {
+  class: "hello",
+  id: "hello",
+};
 const buttonDisabled = true;
 </script>
 
