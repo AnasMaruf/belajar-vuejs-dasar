0b30250dd9afbad67d1c7b01a551c4125579d9cf
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-30 22:22:06 +0700

    Counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 id="count">Counter : {{ count }}</h1>
+    <button v-on:click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+let count = 0;
+function increment() {
+  count++;
+
+  document.getElementById("count").innerText = `Counter : ${count}`;
+}
+</script>
+
+<style scoped></style>
