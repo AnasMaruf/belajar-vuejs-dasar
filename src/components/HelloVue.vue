1f19a6523a28dab11062c8cba626048f73c74788
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-30 21:04:58 +0700

    Initial Commit

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 :id="id" v-bind:class="classHello">{{ hello }}</h1>
+  <div :class="classHello" v-html="hello"></div>
+  <button :disabled="buttonDisabled">This is button</button>
+</template>
+
+<script setup>
+console.log("Hello Vue");
+const hello = "<b>Hello Vue</b>";
+const classHello = "hello";
+const id = "hello";
+const buttonDisabled = true;
+</script>
+
+<style scoped>
+.hello {
+  color: green;
+}
+</style>
