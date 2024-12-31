ad29e864147aae13f25b95ccfbf35e08726fcc51
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-31 20:56:08 +0700

    Reactive

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <h1 id="count">Counter : {{ count }}</h1>
+    <h1 id="count">Counter {{ counter.name }} : {{ counter.count }}</h1>
     <button v-on:click="increment">Increment</button>
   </div>
 </template>
 
 <script setup>
-let count = 0;
-function increment() {
-  count++;
+import { ref } from "vue";
 
-  document.getElementById("count").innerText = `Counter : ${count}`;
+let counter = ref({
+  count: 0,
+  name: "Anas",
+});
+function increment() {
+  console.log(`Increment counter: ${counter.value.count}`);
+  counter.value = {
+    name: counter.value.name,
+    count: counter.value.count + 1,
+  };
 }
 </script>
 
