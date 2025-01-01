428b627dcafc0eeed2f4685c87310420f32ccead
AnasMaruf <anasmaruf1101@gmail.com>
2025-01-01 20:25:02 +0700

    List Rendering with looping

---
+++ src/components/Todolist.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Todolist</h1>
+  <ul>
+    <li v-for="item in items" :key="item.id">{{ item.text }}</li>
+  </ul>
+</template>
+
+<script setup>
+const items = [
+  {
+    id: 1,
+    text: "Learn Vue 3",
+  },
+  {
+    id: 2,
+    text: "Master Vue Router",
+  },
+  {
+    id: 3,
+    text: "Learn Vue Pinia",
+  },
+];
+</script>
+
+<style lang="scss" scoped></style>
